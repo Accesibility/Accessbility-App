7755100731da785228011a5a89415e4f31396a62
Mir Ahmed <mirahmed753@gmail.com>
2018-07-21 11:06:28 -0700

    can get messages, and save as mp3 playable file

--- index.js
+++ index.js
@@ -1,23 +1,127 @@
-'use strict';
+// Load environment variables from `.env` file (optional)
+require('dotenv').config();
+
+const slackEventsApi = require('@slack/events-api');
+const SlackClient = require('@slack/client').WebClient;
+const passport = require('passport');
+const SlackStrategy = require('@aoberoi/passport-slack').default.Strategy;
+const http = require('http');
 const express = require('express');
 const bodyParser = require('body-parser');
+
+var googleTTS = require('google-tts-api');
+var fs = require('fs');
+var player = require('play-sound')(opts = {});
+
+// googleTTS('Hello World', 'en', 1)   // speed normal = 1 (default), slow = 0.24
+// .then(function (url) {
+//   console.log(url); // https://translate.google.com/translate_tts?...
+// })
+// .catch(function (err) {
+//   console.error(err.stack);
+// });
+
+// *** Initialize event adapter using verification token from environment variables ***
+const slackEvents = slackEventsApi.createSlackEventAdapter(process.env.SLACK_VERIFICATION_TOKEN, {
+  includeBody: true
+});
+
+// use web in place of slack for future transactions
+const web = new SlackClient(process.env.SLACK_AUTHORIZATION_TOKEN);
+
+// Initialize a data structures to store team authorization info (typically stored in a database)
+const botAuthorizations = {}
+
+// Helpers to cache and lookup appropriate client
+// NOTE: Not enterprise-ready. if the event was triggered inside a shared channel, this lookup
+// could fail but there might be a suitable client from one of the other teams that is within that
+// shared channel.
+const clients = {};
+function getClientByTeamId(teamId) {
+
+  // console.log(botAuthorizations)
+
+  if (!clients[teamId] && botAuthorizations[teamId]) {
+
+    clients[teamId] = client;
+    return client;
+  }
+
+  return null;
+}
+
+// Initialize an Express application
 const app = express();
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
 
-const server = app.listen(5000, () => {
-  console.log('Express server listening on port %d in %s mode', server.address().port, app.settings.env);});
+// Plug the Add to Slack (OAuth) helpers into the express app
+app.get('/', (req, res) => {
+  res.send('<a href="/auth/slack"><img alt="Add to Slack" height="40" width="139" src="https://platform.slack-edge.com/img/add_to_slack.png" srcset="https://platform.slack-edge.com/img/add_to_slack.png 1x, https://platform.slack-edge.com/img/add_to_slack@2x.png 2x" /></a>');
+});
 
-app.post('/events', (req, res) => {
-  let q = req.body;
-  // 1. To see if the request is coming from Slack
-  if (q.token !== process.env.SLACK_VERIFICATION_TOKEN) {
-    res.sendStatus(400);
-    return;
+// *** Plug the event adapter into the express app as middleware ***
+app.use('/slack/events', slackEvents.expressMiddleware());
+
+// *** Attach listeners to the event adapter ***
+
+// *** Greeting any user that says "hi" ***
+slackEvents.on('message', (message, body) => {
+  // Only deal with messages that have no subtype (plain messages) and contain 'hi'
+  // console.log(body);
+
+  if (!message.subtype && message.text.indexOf('hi') >= 0) {
+    // Initialize a client
+    // const slack = we;
+    // // Handle initialization failure
+    if (!web) {
+      return console.error('No authorization found for this team. Did you install this app again after restarting?');
+    }
+    // Respond to the message back in the same channel
+    web.chat.postMessage({channel: message.channel, text: `Hello <@${message.user}>! :tada:`})
+      .then((res) =>{
+        console.log(res);
+      }).catch(console.error);
   }
-  // 2. Events - get the message text
-  else if (q.type === 'event_callback') {
-    if(!q.event.text) return;
-    analyzeTone(q.event); // sentiment analysis
+});
+
+slackEvents.on('message', (message, body) => {
+  if(!web) {
+    return console.error('We could not handle your message.');
   }
+
+  var file = fs.createWriteStream("file.mp3");
+
+  googleTTS(message.text, 'en', 1)   // speed normal = 1 (default), slow = 0.24
+  .then(function (url) {
+
+  var request = http.get("http://i3.ytimg.com/vi/J---aiyznGQ/mqdefault.jpg", function(response) {
+  response.pipe(file); });
+
+  player.play('file.mp3', function(err) {
+    if (err) throw err
+  });
+
+    console.log(url); // https://translate.google.com/translate_tts?...
+  })
+  .catch(function (err) {
+    console.error(err.stack);
+  });
+
+});
+
+// *** Handle errors ***
+slackEvents.on('error', (error) => {
+  if (error.code === slackEventsApi.errorCodes.TOKEN_VERIFICATION_FAILURE) {
+    // This error type also has a `body` propery containing the request body which failed verification.
+    console.error(`An unverified request was sent to the Slack events Request URL. Request body: \
+${JSON.stringify(error.body)}`);
+  } else {
+    console.error(`An error occurred while handling a Slack event: ${error.message}`);
+  }
+});
+
+// Start the express application
+const port = process.env.PORT || 3000;
+http.createServer(app).listen(port, () => {
+  console.log(`server listening on port ${port}`);
 });
